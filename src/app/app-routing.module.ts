b7afba88a6320283fa2cfcde0b0df1528b029a83
Amancayjesus136 <amancayjesus136@gmail.com>
2024-08-20 01:49:09 -0500

    mudation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './components/views/welcome/welcome.component';
-import { MenuComponent } from './components/layouts/menu/menu.component';
+import { CurriculumVitaeComponent } from './components/views/curriculum-vitae/curriculum-vitae.component';
+import { AboutMeComponent } from './components/views/about-me/about-me.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
+  { path: 'cv', component: CurriculumVitaeComponent },
+  { path: 'sobre/mi', component: AboutMeComponent },
 ];
 
 @NgModule({
